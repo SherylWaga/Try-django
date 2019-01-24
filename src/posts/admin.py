04926c38e7309d94a0a31ad6bb5d7880a7929bda
SherylWaga <sherylwaga@gmail.com>
2019-01-25 00:23:29 +0300

    customize admin panel

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display=["title","updated","timestamp"]
+	list_display_links=["updated"]
+	list_editable=["title"]
+	list_filter=["updated","content"]
+	search_fields=["title","content"]
+	class Meta:
+		model= Post
+admin.site.register(Post,PostAdmin)
