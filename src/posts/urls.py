5891b3fc9c32e26517b2239dea57bfc1ad2dac5e
SherylWaga <sherylwaga@gmail.com>
2019-02-27 12:51:28 +0300

    create a  form to create and edit posts

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('create', views.post_create, name='post_create'),
     path('<int:id>', views.post_detail, name='detail'),
-    path('update', views.post_update, name='post_update'),
+    path('<int:id>/edit', views.post_update, name='post_update'),
     path('delete', views.post_delete, name='post_delete'),
    
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+
 # Create your views here.
 from .models import Post
+from .forms import PostForm
 def post_create(request):
-	return HttpResponse("<h1>HELLO<h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_obsolute_url())
+	content = {
+		"form":form,
+	}
+	return render(request,"post_form.html",content)
 def post_detail(request, id):
 	instance = get_object_or_404(Post,id=id)
 	content = {
@@ -18,7 +28,18 @@ def post_list(request):
 	     "title":"list"
 	}
 	return render(request,"index.html",content)
-def post_update(request):
-	return HttpResponse("<h1>update<h1>")
+def post_update(request,id):
+	instance = get_object_or_404(Post,id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_obsolute_url())
+	content = {
+		"title":instance.title,
+		"content":instance,
+		"form":form,
+	}
+	return render(request,"post_form.html",content)
 def post_delete(request):
 	return HttpResponse("<h1>delete<h1>")
