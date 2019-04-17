8973e34b6b565d37019b614c07077aa6dc297768
SherylWaga <sherylwaga@gmail.com>
2019-04-17 17:17:58 +0300

    heroku hosting

--- src/trydjango2/settings.py
+++ src/trydjango2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +131,4 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+
